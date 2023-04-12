f765f74042c515a0227c1b3c3d52560059a73b16
Danil Lamov <Danil_lamov@mail.ru>
2023-04-12 17:49:47 +0300

    updated sub state

--- Assets/Lamov/StateMachine/Runtime/SubStateMachines/UpdateSubStateMachine.cs
+++ Assets/Lamov/StateMachine/Runtime/SubStateMachines/UpdateSubStateMachine.cs
@@ -3,10 +3,14 @@ using Lamov.StateMachine.Runtime.UpdateStateMachineModule;
 
 namespace Lamov.StateMachine.Runtime.SubStateMachines
 {
-    public class UpdateSubStateMachine : SimpleSubStateMachine<UpdateStateMachine>, IUpdateState, IFixedUpdateState, ILateUpdateState
+    public class UpdateSubStateMachine : SimpleSubStateMachine<UpdateStateMachine>, IUpdateState, IFixedUpdateState, ILateUpdateState, IStateExit
     {
         public void Update() => _stateMachine.Update();
         public void FixedUpdate() => _stateMachine.FixedUpdate();
         public void LateUpdate() => _stateMachine.LateUpdate();
+        public void OnStateExit()
+        {
+            if (_stateMachine.ActiveState is IStateExit exitState) exitState.OnStateExit();
+        }
     }
 }
