59272e82d3bad90c78e3a3eee28e03effe5e667e
Danil Lamov <Danil_lamov@mail.ru>
2023-04-12 17:55:47 +0300

    updated sub states

--- Assets/Lamov/StateMachine/Runtime/SubStateMachines/SimpleSubStateMachine.cs
+++ Assets/Lamov/StateMachine/Runtime/SubStateMachines/SimpleSubStateMachine.cs
@@ -2,13 +2,18 @@
 
 namespace Lamov.StateMachine.Runtime.SubStateMachines
 {
-    public class SimpleSubStateMachine<TStateMachine> : IState where TStateMachine : SimpleStateMachine, new()
+    public class SimpleSubStateMachine<TStateMachine> : IState, IStateExit where TStateMachine : SimpleStateMachine, new()
     {
         protected TStateMachine _stateMachine;
         
         public SimpleSubStateMachine()
         {
             _stateMachine = new TStateMachine();
+        }
+
+        public void OnStateExit()
+        {
+            if (_stateMachine.ActiveState is IStateExit exitState) exitState.OnStateExit();
         }
     }
 }
--- Assets/Lamov/StateMachine/Runtime/SubStateMachines/UpdateSubStateMachine.cs
+++ Assets/Lamov/StateMachine/Runtime/SubStateMachines/UpdateSubStateMachine.cs
@@ -3,14 +3,10 @@ using Lamov.StateMachine.Runtime.UpdateStateMachineModule;
 
 namespace Lamov.StateMachine.Runtime.SubStateMachines
 {
-    public class UpdateSubStateMachine : SimpleSubStateMachine<UpdateStateMachine>, IUpdateState, IFixedUpdateState, ILateUpdateState, IStateExit
+    public class UpdateSubStateMachine : SimpleSubStateMachine<UpdateStateMachine>, IUpdateState, IFixedUpdateState, ILateUpdateState
     {
         public void Update() => _stateMachine.Update();
         public void FixedUpdate() => _stateMachine.FixedUpdate();
         public void LateUpdate() => _stateMachine.LateUpdate();
-        public void OnStateExit()
-        {
-            if (_stateMachine.ActiveState is IStateExit exitState) exitState.OnStateExit();
-        }
     }
 }
